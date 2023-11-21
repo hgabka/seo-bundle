5255b6cbb236c9b794956cf6d9d36bf4e2bef554
hgabka <hgabka@gmail.com>
2023-11-21 18:36:01 +0100

    Update Seo.php

--- src/Entity/Seo.php
+++ src/Entity/Seo.php
@@ -24,181 +24,77 @@ use Symfony\Component\Validator\Constraints as Assert;
 #[ORM\ChangeTrackingPolicy('DEFERRED_EXPLICIT')]
 class Seo
 {
-    /**
-     * @ORM\Id
-     * @ORM\Column(type="bigint")
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
     #[ORM\Id]
-    #[ORM\Column(type: 'bigint')]
+    #[ORM\Column(type: 'integer')]
     #[ORM\GeneratedValue(strategy: 'AUTO')]
     protected ?int $id = null;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="meta_title", type="string", nullable=true)
-     * @Assert\Length(max=65)
-     */
+
     #[ORM\Column(name: 'meta_title', type: 'string', nullable: true)]
     #[Assert\Length(max: 65)]
     protected ?string $metaTitle = null;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="meta_description", type="text", nullable=true)
-     * @Assert\Length(max=155)
-     */
     #[ORM\Column(name: 'meta_description', type: 'text', nullable: true)]
     #[Assert\Length(max: 155)]
     protected ?string $metaDescription = null;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="meta_author", type="string", nullable=true)
-     */
     #[ORM\Column(name: 'meta_author', type: 'string', nullable: true)]
     protected ?string $metaAuthor = null;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="meta_robots", type="string", nullable=true)
-     */
     #[ORM\Column(name: 'meta_robots', type: 'string', nullable: true)]
     protected ?string $metaRobots = null;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="og_type", type="string", nullable=true)
-     */
     #[ORM\Column(name: 'og_type', type: 'string', nullable: true)]
     protected ?string $ogType = null;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="og_title", type="string", nullable=true)
-     */
     #[ORM\Column(name: 'og_title', type: 'string', nullable: true)]
     protected ?string $ogTitle = null;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="og_description", type="text", nullable=true)
-     */
     #[ORM\Column(name: 'og_description', type: 'text', nullable: true)]
     protected ?string $ogDescription = null;
 
-    /**
-     * @var Media
-     *
-     * @ORM\ManyToOne(targetEntity="Hgabka\MediaBundle\Entity\Media")
-     * @ORM\JoinColumn(name="og_image_id", referencedColumnName="id")
-     */
     #[ORM\ManyToOne(targetEntity: Media::class)]
     #[ORM\JoinColumn(name: 'og_image_id', referencedColumnName: 'id')]
     protected ?Media $ogImage = null;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="extra_metadata", type="text", nullable=true)
-     */
     #[ORM\Column(name: 'extra_metadata', type: 'text', nullable: true)]
     protected ?string $extraMetadata = null;
 
-    /**
-     * @var int
-     *
-     * @ORM\Column(type="bigint", name="ref_id", nullable=true)
-     */
-    #[ORM\Column(name: 'ref_id', type: 'bigint', nullable: true)]
+    #[ORM\Column(name: 'ref_id', type: 'integer', nullable: true)]
     protected ?int $refId = null;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(type="string", name="ref_entity_name", nullable=true)
-     */
     #[ORM\Column(name: 'ref_entity_name', type: 'string', nullable: true)]
     protected ?string $refEntityName = null;
-
-    /**
-     * @ORM\Column(type="string", nullable=true, name="og_url")
-     */
+    
     #[ORM\Column(name: 'og_url', type: 'string', nullable: true)]
     protected ?string $ogUrl = null;
 
-    /**
-     * @ORM\Column(type="string", length=100, nullable=true, name="og_article_author")
-     */
     #[ORM\Column(name: 'og_article_author', type: 'string', length: 100, nullable: true)]
     protected ?string $ogArticleAuthor = null;
 
-    /**
-     * @ORM\Column(type="string", length=100, nullable=true, name="og_article_publisher")
-     */
     #[ORM\Column(name: 'og_article_publisher', type: 'string', length: 100, nullable: true)]
     protected ?string $ogArticlePublisher = null;
 
-    /**
-     * @ORM\Column(type="string", length=100, nullable=true, name="og_article_section")
-     */
     #[ORM\Column(name: 'og_article_section', type: 'string', length: 100, nullable: true)]
     protected ?string $ogArticleSection = null;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="twitter_title", type="string", length=255, nullable=true)
-     */
     #[ORM\Column(name: 'twitter_title', type: 'string', length: 255, nullable: true)]
     protected ?string $twitterTitle = null;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="twitter_description", type="text", nullable=true)
-     */
     #[ORM\Column(name: 'twitter_description', type: 'text', nullable: true)]
     protected ?string $twitterDescription = null;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="twitter_site", type="string", length=255, nullable=true)
-     */
     #[ORM\Column(name: 'twitter_site', type: 'string', length: 255, nullable: true)]
     protected ?string $twitterSite = null;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="twitter_creator", type="string", length=255, nullable=true)
-     */
     #[ORM\Column(name: 'twitter_creator', type: 'string', length: 255, nullable: true)]
     protected ?string $twitterCreator = null;
 
-    /**
-     * @var Media
-     *
-     * @ORM\ManyToOne(targetEntity="Hgabka\MediaBundle\Entity\Media")
-     * @ORM\JoinColumn(name="twitter_image_id", referencedColumnName="id")
-     */
     #[ORM\ManyToOne(targetEntity: Media::class)]
     #[ORM\JoinColumn(name: 'twitter_image_id', referencedColumnName: 'id')]
     protected ?Media $twitterImage = null;
 
-    /**
-     * Return string representation of entity.
-     *
-     * @return string
-     */
+
     public function __toString(): string
     {
         return 'hg_seo.admin.label.seo';
