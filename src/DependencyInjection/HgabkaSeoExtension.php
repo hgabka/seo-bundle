93fe0b44824c080b3b07881c14554d4671d22d81
hgabka <hgabka@gmail.com>
2025-02-01 12:07:50 +0100

    Update HgabkaSeoExtension.php

--- src/DependencyInjection/HgabkaSeoExtension.php
+++ src/DependencyInjection/HgabkaSeoExtension.php
@@ -4,9 +4,9 @@ namespace Hgabka\SeoBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\Yaml\Yaml;
 
 /**
